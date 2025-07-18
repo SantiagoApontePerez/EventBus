bcccf669ddbd76a444d7b1d54c4e7cd5f23bee18
SantiagoApontePerez <Lamleecs@outlook.com>
2025-07-18 04:06:58 +0100

    Updated EventBus Debugger.

--- EventBus/Editor/EventBusDebuggerWindow.cs
+++ EventBus/Editor/EventBusDebuggerWindow.cs
@@ -12,6 +12,16 @@ namespace EventBus.EventBus.Editor
 {
     public class EventBusDebuggerWindow : EditorWindow
     {
+        #region Fields
+
+        private Vector2 _scrollPosition;
+        private int _intInput;
+        private float _floatInput;
+        private bool _boolInput;
+        private string _stringInput;
+
+        #endregion
+        
         private void OnGUI()
         {
             _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
@@ -43,6 +53,7 @@ namespace EventBus.EventBus.Editor
             EditorGUILayout.HelpBox(
                 "To begin, create an event/s from the menu located in: "
                 + " `"
+                + "Create/"
                 + InspectorMenus.EventMenu
                 + "` "
                 + "and attach a listener to this event/s",
@@ -52,10 +63,23 @@ namespace EventBus.EventBus.Editor
         private static IEnumerable<ScriptableObject> GetScriptableObjectEvents()
         {
             return AssetDatabase.FindAssets("t:ScriptableObject")
-                .Select(guid => AssetDatabase.LoadAssetAtPath<ScriptableObject>(AssetDatabase.GUIDToAssetPath(guid)))
+                .Select(FindAssetGuid()) 
                 .Where(PredicateEvents());
         }
 
+        /// <summary>
+        /// Selects all scriptable object events and returns its GUID.
+        /// </summary>
+        /// <returns></returns>
+        private static Func<string, ScriptableObject> FindAssetGuid()
+        {
+            return guid => AssetDatabase.LoadAssetAtPath<ScriptableObject>(AssetDatabase.GUIDToAssetPath(guid));
+        }
+
+        /// <summary>
+        /// Returns any defined event.
+        /// </summary>
+        /// <returns></returns>
         private static Func<ScriptableObject, bool> PredicateEvents()
         {
             return e => e
@@ -71,34 +95,22 @@ namespace EventBus.EventBus.Editor
             {
                 case Event<int> intEvent:
                 {
-                    _intInput = EditorGUILayout.IntField("Raise Value", _intInput);
-                    if (GUILayout.Button("Raise")) intEvent.Raise(_intInput);
-                    EditorGUILayout.LabelField("Listeners", intEvent.ListenerCount.ToString());
-                    DrawListenerDetails(intEvent);
+                    DrawEventDetails(intEvent, EditorGUILayout.IntField("Value:", _intInput));
                     break;
                 }
                 case Event<float> floatEvent:
                 {
-                    _floatInput = EditorGUILayout.FloatField("Raise Value", _floatInput);
-                    if (GUILayout.Button("Raise")) floatEvent.Raise(_floatInput);
-                    EditorGUILayout.LabelField("Listeners", floatEvent.ListenerCount.ToString());
-                    DrawListenerDetails(floatEvent);
+                    DrawEventDetails(floatEvent, EditorGUILayout.FloatField("Value:", _floatInput));
                     break;
                 }
                 case Event<bool> boolEvent:
                 {
-                    _boolInput = EditorGUILayout.Toggle("Raise Value", _boolInput);
-                    if (GUILayout.Button("Raise")) boolEvent.Raise(_boolInput);
-                    EditorGUILayout.LabelField("Listeners", boolEvent.ListenerCount.ToString());
-                    DrawListenerDetails(boolEvent);
+                    DrawEventDetails(boolEvent, EditorGUILayout.Toggle("Value:", _boolInput));
                     break;
                 }
                 case Event<string> stringEvent:
                 {
-                    _stringInput = EditorGUILayout.TextField("Raise Value", _stringInput);
-                    if (GUILayout.Button("Raise")) stringEvent.Raise(_stringInput);
-                    EditorGUILayout.LabelField("Listeners", stringEvent.ListenerCount.ToString());
-                    DrawListenerDetails(stringEvent);
+                    DrawEventDetails(stringEvent, EditorGUILayout.TextField("Value:", _stringInput));
                     break;
                 }
             }
@@ -124,7 +136,7 @@ namespace EventBus.EventBus.Editor
 
         private static void DrawSelectListenerObject<T>(string name, IEventListener<T> listener)
         {
-            EditorGUILayout.LabelField(new GUIContent($"{name}", "Listener object name"), EditorStyles.miniLabel);
+            EditorGUILayout.LabelField(new GUIContent($"- {name} -", "Listener object name"), EditorStyles.miniLabel);
 
             if (listener is not Object unityObject || !GUILayout.Button(
                     new GUIContent("Select", "Select this listener in the inspector"),
@@ -134,14 +146,17 @@ namespace EventBus.EventBus.Editor
             EditorGUIUtility.PingObject(unityObject);
         }
 
-        #region Fields
+        private static void DrawRaiseButton<T>(Event<T> @event, T value)
+        {
+            if (GUILayout.Button(new GUIContent("Raise", $"Trigger this event with value: '{value}'"))) 
+                @event.Raise(value);
+        }
 
-        private Vector2 _scrollPosition;
-        private int _intInput;
-        private float _floatInput;
-        private bool _boolInput;
-        private string _stringInput;
-
-        #endregion
+        private static void DrawEventDetails<T>(Event<T> @event, T value)
+        {
+            DrawRaiseButton(@event, value);
+            EditorGUILayout.LabelField("Listeners:", @event.ListenerCount.ToString());
+            DrawListenerDetails(@event);
+        }
     }
 }
--- EventBus/Runtime/Event.cs
+++ EventBus/Runtime/Event.cs
@@ -5,20 +5,32 @@ using UnityEngine;
 
 namespace EventBus.EventBus.Runtime
 {
-    /// <summary>
-    ///     Base class for all events. Responsible for holding and setting the event value.
-    /// </summary>
-    /// <typeparam name="T">Any unmanaged value type.</typeparam>
     public abstract class Event<T> : ScriptableObject
     {
+        // Tracks listeners and their registration time
         private readonly Dictionary<IEventListener<T>, DateTime> _lastInvokeTimes = new();
+        
+        // Tracks last invoke time per listener
         private readonly Dictionary<IEventListener<T>, DateTime> _registrationTimes = new();
 
+        /// <summary>
+        /// Number of active listeners.
+        /// </summary>
         public int ListenerCount => _registrationTimes.Count;
 
+        /// <summary>
+        /// Mapping of listeners to their registration times.
+        /// </summary>
         public IReadOnlyDictionary<IEventListener<T>, DateTime> RegistrationTimes => _registrationTimes;
+        
+        /// <summary>
+        /// Mapping of listeners to their last invoke times.
+        /// </summary>
         public IReadOnlyDictionary<IEventListener<T>, DateTime> LastInvokeTimes => _lastInvokeTimes;
 
+        /// <summary>
+        /// Raises the event with the specified value and recording invoke times.
+        /// </summary>
         public void Raise(T value)
         {
             var now = DateTime.Now;
@@ -30,12 +42,18 @@ namespace EventBus.EventBus.Runtime
             }
         }
 
+        /// <summary>
+        /// Registers a listener and stores its registration time.
+        /// </summary>
         public void RegisterListener(IEventListener<T> listener)
         {
             //Registers the listener and adds a timestamp.
             if (!_registrationTimes.ContainsKey(listener)) _registrationTimes[listener] = DateTime.Now;
         }
 
+        /// <summary>
+        /// Unregisters a listener, removing its records.
+        /// </summary>
         public void UnregisterListener(IEventListener<T> listener)
         {
             if (!_registrationTimes.Remove(listener)) return;
